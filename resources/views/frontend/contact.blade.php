ec95f376e43a7f9efa94d930c471120d608b8fe1
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-01-09 19:35:18 +0600

    map added

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -45,8 +45,21 @@
 
         </div>
     </section>
-
-
-    
     <!-- Branch section end -->
+
+    <!-- Map section Start -->
+    <section class="map-section py-120">
+        <div class="container">
+            <div class="section-title text-center mb-5">
+                <h4 class="sub-title">Our Location on Map</h4>
+                <h2 class="title">
+                    Find Our Location on Map
+                </h2>
+            </div>
+            <!-- map -->
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3647.348832385361!2d90.25773797590065!3d23.91269868252525!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755e9edefc0469d%3A0x405b65d31a1f445e!2sSENA%20Shopping%20Complex!5e0!3m2!1sen!2sbd!4v1736429589330!5m2!1sen!2sbd" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <!-- map -->
+        </div>
+    </section>
+    <!-- map section end -->
 @endsection
