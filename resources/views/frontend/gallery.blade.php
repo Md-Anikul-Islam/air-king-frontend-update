e6376bd3482f1e2be6d612d866f8044d5cc895cc
Mr-Rafiqul-Islam <rafiqulislamemon05@gmail.com>
2025-01-11 17:45:06 +0600

    gallery updated

--- resources/views/frontend/gallery.blade.php
+++ resources/views/frontend/gallery.blade.php
@@ -27,15 +27,14 @@
             <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
                     class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
-
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
-                    class="w-100 shadow-1-strong rounded mb-4" alt="Wintry Mountain Landscape" />
+            </div>
+            
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
             </div>
 
             <div class="col-lg-4 mb-4 mb-lg-0">
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
-                    class="w-100 shadow-1-strong rounded mb-4" alt="Mountains in the Clouds" />
-
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
                     class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
             </div>
@@ -43,9 +42,15 @@
             <div class="col-lg-4 mb-4 mb-lg-0">
                 <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
                     class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
+            </div>
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Boat on Calm Water" />
+            </div>
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
-                    class="w-100 shadow-1-strong rounded mb-4" alt="Yosemite National Park" />
+            <div class="col-lg-4 mb-4 mb-lg-0">
+                <img src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
+                    class="w-100 shadow-1-strong rounded mb-4" alt="Waves at Sea" />
             </div>
         </div>
     </div>
